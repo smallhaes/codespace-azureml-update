b32296ea7b75393570e3e22187d386872b4f9423
Hinata <littlehaes@bupt.edu.cn>
2020-07-01 08:20:57 +0800

    update

--- check_for_update.sh
+++ check_for_update.sh
@@ -1,14 +1,4 @@
 #!/bin/bash
-# 激活base环境
-#~/conda/bin/conda activate base
-# 获取当前的sdk版本
-CUR_VERSION=`pip show azureml-pipeline-wrapper | grep Version | cut -d "." -f 4`
-# 获取最新的sdk版本
-NEW_VERSION=`curl -s https://versionofsdk.blob.core.windows.net/versionofsdk/version.txt`
-# 如果版本没有变化则不用更新
-if [[ $NEW_VERSION == $CUR_VERSION ]]; then
-       exit
-fi
 
 #.sdk_update_lock_dir目录下的sdk_update_lock用于记录上一次检查更新的时间
 SDK_UPDATE_LOCK_DIR=~/.sdk_update_lock_dir
@@ -34,10 +24,22 @@ function _last_epoch() {
 epoch_interval_min=-2
 
 #计算当前时间距离上次检查更新的时间
-
 epoch_interval=$(($(_current_epoch) - $(_last_epoch)))
 
-if [[ $epoch_interval -gt $epoch_interval_min ]] && [[ $NEW_VERSION > $CUR_VERSION ]]; then
+if [[ $epoch_interval -le $epoch_interval_min ]]; then
+       exit
+fi
+
+# 获取当前的sdk版本
+CUR_VERSION=`pip show azureml-pipeline-wrapper | grep Version | cut -d "." -f 4`
+# 获取最新的sdk版本
+NEW_VERSION=`curl -s https://versionofsdk.blob.core.windows.net/versionofsdk/version.txt`
+# 如果版本没有变化则不用更新
+if [[ $NEW_VERSION == $CUR_VERSION ]]; then
+       exit
+fi
+
+if [[ $NEW_VERSION > $CUR_VERSION ]]; then
         echo -e "Found new releases of Azure Module SDK which take about 15 minutes to update. Would you like to  update them right now? [Y/n]: \c"
         read line
         if [[ "$line" == Y* ]] || [[ "$line" == y* ]] || [ -z "$line" ]; then
