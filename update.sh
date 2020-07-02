5e761d4c1060fb82a790c65fe80737ef45305ff3
Hinata <littlehaes@bupt.edu.cn>
2020-07-02 15:28:01 +0800

    update

--- update.sh
+++ update.sh
@@ -9,31 +9,7 @@ AZ_EXTENSION_SOURCE=https://azuremlsdktestpypi.azureedge.net/Pipeline-Wrapper-SD
 PATH_SITE_PACKAGES=/home/vsonline/.local/lib/python3.7/site-packages/
 PIP_PATH=/usr/local/bin/pip
 
-tmp=`ls -d ${PATH_SITE_PACKAGES}azureml_defaults*`
-CUR_VERSION_AZUREML_DEFAULTS=${tmp:0-18:8}
-tmp=`ls -d ${PATH_SITE_PACKAGES}azureml_pipeline_core*`
-CUR_VERSION_AZUREML_PIPELINE_CORE=${tmp:0-18:8}
-tmp=`ls -d ${PATH_SITE_PACKAGES}azureml_pipeline_wrapper*`
-CUR_VERSION_AZUREML_PIPELINE_WRAPPER=${tmp:0-18:8}
-MIN=$CUR_VERSION_AZUREML_DEFAULTS
-if [[ $MIN > $CUR_VERSION_AZUREML_PIPELINE_CORE ]]; then
-    MIN=$CUR_VERSION_AZUREML_PIPELINE_CORE
-fi
-if [[ $MIN > $CUR_VERSION_AZUREML_PIPELINE_WRAPPER ]]; then
-    MIN=$CUR_VERSION_AZUREML_PIPELINE_WRAPPER
-fi
-
-if [[ $SDK_VERSION_SHORT > $CUR_VERSION_AZUREML_DEFAULTS ]]; then
-	$PIP_PATH install -U --extra-index-url=$SDK_SOURCE azureml-defaults==$SDK_VERSION_LONG
-fi
-
-if [[ $SDK_VERSION_SHORT > $CUR_VERSION_AZUREML_PIPELINE_CORE ]]; then
-	$PIP_PATH install -U --extra-index-url=$SDK_SOURCE azureml-pipeline-core==$SDK_VERSION_LONG
-fi
-
-if [[ $SDK_VERSION_SHORT > $CUR_VERSION_AZUREML_DEFAULTS ]]; then
-	$PIP_PATH install -U --extra-index-url=$SDK_SOURCE azureml-pipeline-wrapper[notebooks]==$SDK_VERSION_LONG
-fi
+$PIP_PATH install -U --extra-index-url=$SDK_SOURCE azureml-defaults==$SDK_VERSION_LONG azureml-pipeline-core==$SDK_VERSION_LONG azureml-pipeline-wrapper[notebooks]==$SDK_VERSION_LONG
 
 tmp=`cat ${PATH_SITE_PACKAGES}azureml/_base_sdk_common/_version.py`
 CUR_VERSION_AZURE_CLI_ML=${tmp:0-10:8}
